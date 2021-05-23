2e6d03feaa7d1a181930b24c4657703c583b706a
Sereyroath <kimsereyroth@yahoo.com>
2021-05-24 01:33:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from pusher import Pusher
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
-    
+import pymysql
+pymysql.install_as_MySQLdb()
+
 app = Flask(__name__)
 app.secret_key = "Secret Key"
 
